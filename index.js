fd81e74ca798112f3bf3394c9270cb2a7c06f02d
Cherkashyn Dmytro <98047651+Pozitiv69@users.noreply.github.com>
2022-10-23 11:13:03 +0300

    +options

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const TelegramBot = require('node-telegram-bot-api');
 const path = require('node:path');
-
+const { gameOption, gameButton, againOption } = require('./options.js');
 const token = '5666972940:AAGagdTZEU0MaA1Trdk3MWiQSMnEBSNJ6IY';
 
 const bot = new TelegramBot(token, { polling: true });
@@ -13,42 +13,6 @@ bot.setMyCommands([
   { command: '/game', description: 'Игра: Угадай число, ПРИЗ 100$' },
 ]);
 
-const againOption = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [[{ text: 'Играть еще раз!', callback_data: '/again' }]],
-  }),
-};
-
-const gameButton = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [[{ text: 'Играть!', callback_data: '/again' }]],
-  }),
-};
-
-const gameOption = {
-  reply_markup: JSON.stringify({
-    inline_keyboard: [
-      [
-        { text: '1', callback_data: '1' },
-        { text: '2', callback_data: '2' },
-        { text: '3', callback_data: '3' },
-      ],
-      [
-        { text: '4', callback_data: '4' },
-        { text: '5', callback_data: '5' },
-        { text: '6', callback_data: '6' },
-      ],
-
-      [
-        { text: '7', callback_data: '7' },
-        { text: '8', callback_data: '8' },
-        { text: '9', callback_data: '9' },
-      ],
-      [{ text: '0', callback_data: '0' }],
-    ],
-  }),
-};
-
 const startGame = async (chatId) => {
   await bot.sendMessage(
     chatId,
@@ -59,7 +23,7 @@ const startGame = async (chatId) => {
   await bot.sendMessage(chatId, 'Отгадывай', gameOption);
 };
 
-const start = () => {
+const startBot = () => {
   bot.on('message', async (msg) => {
     const chatId = msg.chat.id;
     const text = msg.text;
@@ -70,32 +34,37 @@ const start = () => {
         `Привет я тебя знаю! Вот немного инфы про тебя /info`
       );
     }
+
     if (text === '/info') {
-      await bot.sendMessage(
+      bot.sendMessage(
         chatId,
         `Тебя звать ${msg.from.first_name}!\nЭто твое фото? Если нет, то давай сыграем.`
       );
       return bot.sendPhoto(chatId, './file/IMG_kep.png', gameButton);
     }
+
     if (text === '/game') {
       return startGame(chatId);
     }
+
     return bot.sendMessage(chatId, `Я тебя не понимаю`);
   });
 
   bot.on('callback_query', async (msg) => {
     const data = msg.data;
     const chatId = msg.message.chat.id;
+
     if (data === '/again') {
       return startGame(chatId);
     }
-    if (data === chats[chatId])
+
+    if (data == chats[chatId]) {
       return bot.sendMessage(
         chatId,
         `Поздравляю ты отгадал цифру ${chats[chatId]}`,
         againOption
       );
-    else {
+    } else {
       return bot.sendMessage(
         chatId,
         `Ты не отгадал, правильный ответ был ${chats[chatId]}`,
@@ -105,4 +74,4 @@ const start = () => {
   });
 };
 
-start();
+startBot();
